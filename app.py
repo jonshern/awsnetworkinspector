ab35e1b6ef5a1a0d2286586258f4c19aff80305e
Jon Shern <jonshern@outlook.com>
2017-05-20 23:18:29 -0500

    added a bunch of stuff

--- app.py
+++ app.py
@@ -24,10 +24,61 @@ class VpnGateway:
         self.__dict__.update(entries)  
 
 class Vpc:
-    Subnets = []
+    id = ''
+    cidr_block = ''
+    dhcp_options_id = ''
+    instance_tenancy = ''
+    ipv6_cidr_block_association_set = ''
+    is_default = ''
+    state = ''
+    tags = ''
+    vpc_id = ''
 
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
+    item = ''
+    subnets = []
+    securitygroups = []
+    
+
+    def __init__(self, item):
+        self.item = item
+
+    def printvpc(self):
+        print (self.item)
+
+
+class ElasticIp:
+    NetworkInterfaceId = ''
+    AssociationId = ''
+    NetworkInterfaceOwnerId = ''
+    PublicIp = ''
+    AllocationId = ''
+    PrivateIpAddress = ''
+
+    def __init__(self, item):
+        if 'NetworkInterfaceId' in item:
+            self.NetworkInterfaceId = item['NetworkInterfaceId']
+
+        if 'AssociationId' in item:
+            self.AssociationId = item['AssociationId']
+
+        if 'NetworkInterfaceOwnerId' in item:
+            self.NetworkInterfaceOwnerId = item['NetworkInterfaceOwnerId']
+
+        if 'PublicIp' in item:
+            self.PublicIp = item['PublicIp']
+
+        if 'AllocationId' in item:
+            self.AllocationId = item['AllocationId']
+
+        if 'PrivateIpAddress' in item:
+            self.PrivateIpAddress = item['PrivateIpAddress']
+
+
+    def printeip(self):
+        print ('----------------------------------')
+        print ('Public IP ' + self.PublicIp)
+        print ('Private IP ' + self.PrivateIpAddress)
+
 
 class Subnet:
     def __init__(self, **entries):
@@ -47,27 +98,60 @@ class Ec2:
 
 def main():
     
-
     parser = argparse.ArgumentParser(
         description='Inspect the network in AWS and create a report')
+
+    parser.add_argument(
+        '-p', '--profile', help='Set the Profile', default='')
+
     parser.add_argument(
         '-i', '--inspect', help='Initiate the inspection of the aws network', action='store_true')
+    parser.add_argument(
+        '-v', '--vpc', help='Get a list of the vpcs', action='store_true')
+    parser.add_argument(
+        '-e', '--elasticip', help='Get a list of the Elasic Ips', action='store_true')
     args = vars(parser.parse_args())
 
-    vpclist = getvpclist()
+
+
+    profilename = args['profile']
 
     if args['inspect']:
         print 'Starting the inspection of the network'
 
+    if args['vpc']:
+        vpclist = getvpclist(profilename)
+        for item in vpclist:
+                print (item.printvpc())
+        # print (str(item))
+
+    if args['elasticip']:
+        getelasticips(profilename)
 
 
 def initializeaccountlist():
     accountlist = []
 
 
-    
+def getelasticips(profilename):
 
-def getvpclist():
+    dev = boto3.session.Session(profile_name=profilename)  
+    ec2 = boto3.client('ec2')
+    filters = [
+        {'Name': 'domain', 'Values': ['vpc']}
+    ]
+    response = ec2.describe_addresses(Filters=filters)
+    
+    items = response['Addresses']
+    for item in items:
+        eip = ElasticIp(item)
+        eip.printeip()
+        # print (item)
+
+
+def getvpclist(profilename):
+
+    dev = boto3.session.Session(profile_name=profilename)  
     """Get a list of all of the vpcs for a given aws account"""
     client = boto3.client('ec2', verify=False)
     response = client.describe_vpcs(
@@ -81,17 +165,13 @@ def getvpclist():
             },
         ]
     )
-
     items = response['Vpcs']
-
     vpcs = []
     firstitem = items[0]
     seconditem = items[1]
     for item in items:
-        vpcobject = AWSVPC(item)
+        vpcobject = Vpc(item)
         vpcs.append(vpcobject)
-
-    # items = response['Vpcs']
 
     return vpcs
 
